c114a79d0e23e42e8ffafe828e690c0b52ca8bb4
Julio Marques <juliomarquesjr@yahoo.com.br>
2021-09-23 05:26:28 -0300

    Ajustes de layout

--- src/components/DataStreaming.jsx
+++ src/components/DataStreaming.jsx
@@ -5,7 +5,6 @@ import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 
 const Item = styled(Paper)(({ theme }) => ({
-  
   padding: theme.spacing(2),
   textAlign: "center",
   marginTop: "20px",
@@ -16,12 +15,16 @@ function DataStreaming(props) {
   return (
     <Item>
       <Stack direction="row" spacing={2}>
-        <Typography variant="h7">
-          <span>Titulo da Transmissão: {props.dataRadio.title} <br /> </span>
-          <span>Bitrate: {props.dataRadio.bitrate}<br /></span>
-          <span>Musica: {props.dataRadio.song}<br /></span>
-          <audio src={props.dataRadio.tuneinurl} controls ></audio>
+        <Typography variant="h6" gutterBottom component="div">
+          Titulo da Transmissão: {props.dataRadio.title}
         </Typography>
+        <Typography variant="h6" gutterBottom component="div">
+        Bitrate: {props.dataRadio.bitrate}
+        </Typography>
+        <Typography variant="h6" gutterBottom component="div">
+        Musica: {props.dataRadio.song}
+        </Typography>
+        <audio src={props.dataRadio.tuneinurl} controls></audio>
       </Stack>
     </Item>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,6 +12,8 @@ import Stack from "@mui/material/Stack";
 import { getDataRadio } from "../functions/getDataRadio";
 import DataStreaming from "./DataStreaming";
 
+import Tooltip from "@mui/material/Tooltip";
+
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(2),
@@ -25,10 +27,10 @@ function Form() {
   const [server, setServer] = useState("r15.ciclano.io");
   const [user, setUser] = useState("rdocari");
 
-  function renderData(){
+  function renderData() {
     getDataRadio(`http://${server}:2199/rpc/${user}/streaminfo.get`).then(
-      returData => setDataRadio(returData)
-    )
+      (returData) => setDataRadio(returData)
+    );
   }
 
   return (
@@ -38,39 +40,40 @@ function Form() {
           <Grid item xs={4}>
             <Item>
               <Stack direction="row" spacing={2}>
-                <TextField
-                  id="outlined-basic-normal"
-                  label="Servidor"
-                  value={server}
-                  fullWidth
-                  variant="outlined"
-                  onChange={(event) => setServer(event.target.value)}
-                />
-
-                <TextField
-                  id="outlined-basic-normal"
-                  label="Usuário"
-                  variant="outlined"
-                  value={user}
-                  onChange={(event) => setUser(event.target.value)}
-                />
+                <Tooltip title="Informe o dominio ou ip do seu streaming sem o http ou https">
+                  <TextField
+                    id="outlined-basic-normal"
+                    label="Servidor"
+                    value={server}
+                    fullWidth
+                    variant="outlined"
+                    onChange={(event) => setServer(event.target.value)}
+                  />
+                </Tooltip>
+                <Tooltip title="Informe o usuário de login com o painel Centova Cast">
+                  <TextField
+                    id="outlined-basic-normal"
+                    label="Usuário"
+                    variant="outlined"
+                    value={user}
+                    onChange={(event) => setUser(event.target.value)}
+                  />
+                </Tooltip>
               </Stack>
-            
+
               <Stack direction="column" spacing={1}>
-                <Button
-                  variant="outlined"
-                  startIcon={<DeleteIcon />}
-                >
+                <Button variant="outlined" startIcon={<DeleteIcon />}>
                   Limpar
                 </Button>
-
-                <Button
-                  variant="contained"
-                  onClick={() => renderData()}
-                  startIcon={<SendIcon />}
-                >
-                  Verificar
-                </Button>
+                <Tooltip title="Envie os dados para pesquisar" arrow>
+                  <Button
+                    variant="contained"
+                    onClick={() => renderData()}
+                    startIcon={<SendIcon />}
+                  >
+                    Verificar
+                  </Button>
+                </Tooltip>
               </Stack>
             </Item>
           </Grid>
