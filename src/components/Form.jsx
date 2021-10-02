4c3fe6bf9a8b1602d6f6bfba95fe7d1887ecf2c7
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2021-10-02 18:10:28 -0300

    Add Componet Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,28 @@
 import React from "react"
+import { Grid, Card, Input,Spacer, Button } from "@geist-ui/react";
+import { Database } from "@geist-ui/react-icons";
 
 const Form = props =>(
     <React.Fragment>
-        
+        <Grid xs={22} md={10}>
+        <Card shadow width="100%" height="350px">
+          <Input placeholder="IP our Domains Server" scale={4 / 3} width="95%">
+            Domain ou IP Server Centova Cast
+          </Input>
+          <Spacer h={3} />
+          <Input
+            placeholder="User Account Centova Cast"
+            scale={4 / 3}
+            width="95%"
+          >
+            Username your Centova Cast
+          </Input>
+          <Spacer h={3} />
+          <Button icon={<Database />} type="success">
+            Verify Account
+          </Button>
+        </Card>
+      </Grid>
     </React.Fragment>
 )
 export default Form
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,31 +1,13 @@
 import React from "react";
 import { Grid, Card, Input,Spacer, Button, Loading } from "@geist-ui/react";
-import { Database } from "@geist-ui/react-icons";
 import Header from "../components/Header";
+import Form from "../components/Form";
 
-const Home = (props) => (
+const Home = () => (
   <React.Fragment>
     <Grid.Container gap={2} justify="center">
       <Header titleHome="Centova Cast - Verify Status Account" />
-      <Grid xs={22} md={10}>
-        <Card shadow width="100%" height="350px">
-          <Input placeholder="IP our Domains Server" scale={4 / 3} width="95%">
-            Domain ou IP Server Centova Cast
-          </Input>
-          <Spacer h={3} />
-          <Input
-            placeholder="User Account Centova Cast"
-            scale={4 / 3}
-            width="95%"
-          >
-            Username your Centova Cast
-          </Input>
-          <Spacer h={3} />
-          <Button icon={<Database />} type="success">
-            Verify Account
-          </Button>
-        </Card>
-      </Grid>
+      <Form />
       <Grid xs={22} md={10}>
         <Card shadow width="100%">
           <Loading>Aguardando informações</Loading>
