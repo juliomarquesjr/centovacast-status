04c1e0824a8341eec7554e32debad929fa2adecd
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2021-10-07 15:58:02 -0300

    Add loading

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,37 +1,44 @@
-import React from "react"
-import { Grid, Card, Input,Spacer, Button } from "@geist-ui/react";
+import React from "react";
+import { Grid, Card, Input, Spacer, Button } from "@geist-ui/react";
 import { Database } from "@geist-ui/react-icons";
 
-const Form = props =>(
-    <React.Fragment>
-        <Grid xs={22} md={10}>
-        <Card shadow width="100%" height="350px">
-          <Input placeholder="IP our Domains Server" 
-          scale={4 / 3} 
+const Form = (props) => (
+  <React.Fragment>
+    <Grid xs={22} md={10}>
+      <Card shadow width="100%" height="350px">
+        <Input
+          placeholder="IP our Domains Server"
+          scale={4 / 3}
           width="95%"
           value={props.values.host}
-          onChange={event => props.setDataForm({...props.values, host: event.target.value})}
-          >
-            Domain ou IP Server Centova Cast
-          </Input>
-          <Spacer h={3} />
-          <Input
-            placeholder="User Account Centova Cast"
-            scale={4 / 3}
-            width="95%"
-            value={props.values.user}
-            onChange={event => props.setDataForm({...props.values, user: event.target.value})}
-          >
-            Username your Centova Cast
-          </Input>
-          <Spacer h={3} />
-          <Button icon={<Database />} 
+          onChange={(event) =>
+            props.setDataForm({ ...props.values, host: event.target.value })
+          }
+        >
+          Domain ou IP Server Centova Cast
+        </Input>
+        <Spacer h={3} />
+        <Input
+          placeholder="User Account Centova Cast"
+          scale={4 / 3}
+          width="95%"
+          value={props.values.user}
+          onChange={(event) =>
+            props.setDataForm({ ...props.values, user: event.target.value })
+          }
+        >
+          Username your Centova Cast
+        </Input>
+        <Spacer h={3} />
+        <Button
+          icon={<Database />}
           type="success"
-          onClick={() => props.verifyDataJson()}>
-            Verify Account
-          </Button>
-        </Card>
-      </Grid>
-    </React.Fragment>
-)
-export default Form
+          onClick={() => props.verifyDataJson()}
+        >
+          Verify Account
+        </Button>
+      </Card>
+    </Grid>
+  </React.Fragment>
+);
+export default Form;
